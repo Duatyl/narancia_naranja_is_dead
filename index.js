83300dbb6bba86066238bc0af534278ef6a5d636
Duatyl <noodlenoob2@gmail.com>
2026-04-14 15:28:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello, didn't know you knew how to look at the source code, do you know more than I do? Am I just a fool? What other SECRETS DO YOU HAVE?");
+
+window.alert("There are now 4283 viruses in your device");
